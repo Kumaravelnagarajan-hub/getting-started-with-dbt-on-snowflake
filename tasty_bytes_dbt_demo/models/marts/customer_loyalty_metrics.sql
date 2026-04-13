0de6a391ea8b9b424461e50a822d9d11d738a0e1
Kumaravelnagarajan-hub <harish.kumar.tholasi@accenture.com>
2026-04-13 09:13:38 +0000

    small changes

--- tasty_bytes_dbt_demo/models/marts/customer_loyalty_metrics.sql
+++ tasty_bytes_dbt_demo/models/marts/customer_loyalty_metrics.sql
@@ -6,6 +6,7 @@ SELECT
     cl.last_name,
     cl.phone_number,
     cl.e_mail,
+    COUNT(oh.order_id) AS total_orders,
     SUM(oh.order_total) AS total_sales,
     ARRAY_AGG(DISTINCT oh.location_id) AS visited_location_ids_array
 FROM {{ ref('raw_customer_customer_loyalty') }} cl
@@ -13,3 +14,4 @@ JOIN {{ ref('raw_pos_order_header') }} oh
 ON cl.customer_id = oh.customer_id
 GROUP BY cl.customer_id, cl.city, cl.country, cl.first_name,
 cl.last_name, cl.phone_number, cl.e_mail
+ORDER BY total_sales DESC
